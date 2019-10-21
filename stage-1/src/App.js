c4b0561ebc2ce6ba6a72f56e706dad8b05763c2e
Mimi <mimitraugott@gmail.com>
2019-10-21 16:39:15 -0600

    afternoon debug with Fred

--- stage-1/src/App.js
+++ stage-1/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import StoreFront from "./Components/StoreFront/StoreFront";
 import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
@@ -17,7 +18,7 @@ class App extends Component {
   }
   componentDidMount() {
     axios
-      .get("https://practiceapi.devmountain.com/products/")
+    .get("https://practiceapi.devmountain.com/products/")
       .then(response => {
         this.setState({
           products: response.data
@@ -48,15 +49,15 @@ class App extends Component {
     }
   }
   render() {
-    const { products, showCart } = this.state;
+    const { products, showCart, cart } = this.state;
     return (
       <div className="App">
         <NavBar navigate={this.navigate} />
         <div className="main-container">
           {showCart ? (
-            <ShoppingCart cart={cart} />
+            <ShoppingCart cart={cart} removeFromCart = {this.removeFromCart} />
           ) : (
-            <StoreFront products={products} />
+            <StoreFront products={products} addToCart = {this.addToCart} />
           )}
         </div>
       </div>
